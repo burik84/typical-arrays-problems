52232628c506b9a81c66b539c6f90681f6e2a293
burikAV <AVburik84@gmail.com>
2020-03-12 10:42:55 +0300

    feat: add script solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
+exports.min = function min(array) {
+    if (!(array instanceof Array) || array.length === 0) {
+        console.log(0);
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+
+    // console.log(array, min);
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!(array instanceof Array) || array.length === 0) {
+        console.log(0);
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+    // console.log(array, max);
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!(array instanceof Array) || array.length === 0) {
+        console.log(0);
+        return 0;
+    }
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+    // avg = avg / array.length;
+    // console.log(array, avg);
+    return avg / array.length;
 }
